bf73a6e839c4becc2619b8ee77605ac4aff4292a
Omri Steinberg <omristg@gmail.com>
2022-03-27 04:57:23 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,9 @@ app.use('/api/ticket', ticketRoutes)
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-    // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    // FIX: below code fixes app crashing on refresh in deployment
+    
     app.get('*', (_, res) => {
         res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
     })
